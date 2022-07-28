659a34c3270b786cff091d303fc6a50f46c5136e
Barçın <68463413+lbarcl@users.noreply.github.com>
2022-07-28 14:41:52 +0300

    Update index.svelte

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,6 @@
 <svelte:head>
     <title>D mi, C mi?</title>
-    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7256206519126120" crossorigin="anonymous"></script>
+    <meta name="hilltopads-site-verification" content="b73b242b031fc3ea86056bc777f997d31d0f2503" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
